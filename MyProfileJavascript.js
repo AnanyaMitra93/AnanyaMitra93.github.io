7689dd6cb9828f8cc09a633d38699850d40e6a8d
AnanyaMitra93 <33443391+AnanyaMitra93@users.noreply.github.com>
2017-11-07 22:25:36 +0530

    profile page

---
+++ MyProfileJavascript.js
@@ -0,0 +1,7 @@
+﻿function navbar_open() {
+    document.getElementById("myNavBar").style.display = "block";
+}
+
+function navbar_close() {
+    document.getElementById("myNavBar").style.display = "none";
+}

e697cd739e9131e57eaab30c0c6472150109db4e
AnanyaMitra93 <33443391+AnanyaMitra93@users.noreply.github.com>
2017-11-08 09:09:56 +0530

    Update MyProfileJavascript.js

--- MyProfileJavascript.js
+++ MyProfileJavascript.js
@@ -5,3 +5,53 @@
 function navbar_close() {
     document.getElementById("myNavBar").style.display = "none";
 }
+
+$(document).ready(function () {
+
+    $("#knowMore").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#aboutMe").offset().top
+        }, 1000);
+    });
+
+    $("#toHome").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#homePage").offset().top
+        }, 1000);
+    });
+
+    $("#toAboutMe").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#aboutMe").offset().top
+        }, 1000);
+    });
+
+    $("#toEducation").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#education").offset().top
+        }, 1000);
+    });
+
+    $("#toTechnology").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#technology").offset().top
+        }, 1000);
+    });
+
+    $("#toProject").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#project").offset().top
+        }, 1000);
+    });
+    $("#toContact").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+
+    $("#toTop").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#homePage").offset().top
+        }, 1000);
+    });
+})
